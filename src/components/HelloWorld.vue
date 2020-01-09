b01b94002a2a47d744983e8139790bca4a894bb3
sh6616 <15539550863@163.com>
2020-01-09 16:34:32 +0800

    1

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <h3>Ecosystem</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+</style>
---
+++ src/views/login/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>1231</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped></style>
